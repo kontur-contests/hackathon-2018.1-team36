e1bb87bcd9fbe5480a5878ba14be8a86252b6e64
Gosha Krikun <g.krikun@innopolis.ru>
2018-04-07 19:04:05 +0300

    Add Bullet, fix hashmap

---
+++ scenario.js
@@ -0,0 +1,15 @@
+const Game = require('./lib/engine/Game');
+
+const game = new Game(1000, 1000);
+
+const tank_id = game.createTank();
+
+const tank = game.getTank(tank_id);
+
+console.log(game);
+
+tank.setFire(1);
+
+game.update(1);
+
+console.log(game);
