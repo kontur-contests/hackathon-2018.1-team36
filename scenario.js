2442a1a4960804fce60a9c2c3c7e914bf79f188b
Gosha Krikun <g.krikun@innopolis.ru>
2018-04-07 22:04:44 +0300

    Add bullets checking

--- scenario.js
+++ scenario.js
@@ -2,25 +2,23 @@ const Game = require('./lib/engine/Game');
 
 const game = new Game(1000, 1000);
 
-const tank_id = game.createTank();
+const tank_id = game.createTank(500, 500);
 
 const tank = game.getTank(tank_id);
 
-console.log(game);
+function log(game) {
+    const state = game.currentState();
+    // console.log(JSON.stringify(state, null, 2));
+    // console.log(JSON.stringify(state.bullets, null, 2));
+    // console.log(state.bullets.map(b => (b.is_expired())));
+    console.log('==================================================================');
+    console.log(JSON.stringify(state.bullets, null, 2));
+}
 
+tank.setLeftTrackMoving(1);
 tank.setFire(1);
 
-game.update(1);
-game.update(1);
-game.update(1);
-game.update(1);
-game.update(1);
-game.update(1);
-game.update(1);
-game.update(1);
-game.update(1);
-game.update(1);
-game.update(1);
-game.update(1);
-
-console.log(game);
+for (let i = 0; i < 100; i++) {
+    game.update(1);
+    log(game);
+}
